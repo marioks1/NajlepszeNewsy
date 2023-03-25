cbc829276eec604ea990c141f84573c597f1d565
Mariusz Dziuba <mardziuba@gmail.com>
2023-03-25 21:39:24 +0100

    Initial Commit

---
+++ NajlepszeNewsy/NajlepszeNewsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NajlepszeNewsyApp.swift
+//  NajlepszeNewsy
+//
+//  Created by Mariusz Dziuba on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NajlepszeNewsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
