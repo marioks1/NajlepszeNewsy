6d1a77ba017ee24164d08f47ed77765bf5855545
Mariusz Dziuba <mardziuba@gmail.com>
2023-03-26 14:16:42 +0200

    poprawki kosmetyczne - f-cja przytnijsutring

--- NajlepszeNewsy/Model/DanePostu.swift
+++ NajlepszeNewsy/Model/DanePostu.swift
@@ -11,8 +11,8 @@ struct Post: Decodable, Identifiable {
     }
     var sformatowanaData: String {
         let rok = String(publishedAt.prefix(4))
-        let miesiac = przytnijString(stringDoPrzyciecia:String(publishedAt), indexStart: 5, indexEnd: 6)
-        let dzien = przytnijString(stringDoPrzyciecia:String(publishedAt), indexStart: 8, indexEnd: 9)
+        let miesiac = przytnijString(String:String(publishedAt), od: 5, do: 6)
+        let dzien = przytnijString(String:String(publishedAt), od: 8, do: 9)
         let finalnaData = "\(dzien)-\(miesiac)-\(rok)"
         return finalnaData
     }
@@ -21,7 +21,7 @@ struct Post: Decodable, Identifiable {
     let publishedAt: String
     let url: String
     
-    func przytnijString(stringDoPrzyciecia: String, indexStart: Int, indexEnd: Int) -> String {
+    func przytnijString(String stringDoPrzyciecia: String, od indexStart: Int, do indexEnd: Int) -> String {
         let start = stringDoPrzyciecia.index(stringDoPrzyciecia.startIndex, offsetBy: indexStart)
         let end = stringDoPrzyciecia.index(stringDoPrzyciecia.startIndex, offsetBy: indexEnd)
         let range = start...end
